6072308099b751b14f0a81b23a02db522b869ac4
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-02-18 01:05:04 +0200

    Finished Services module

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,18 @@
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
