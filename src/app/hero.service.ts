e9c78f1ad237b7f58d85ae9b0911fff70e828486
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-02-18 18:20:33 +0200

    New

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,9 +10,19 @@ import {Observable, of} from 'rxjs';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
+    this.deleteDuplicateMessage('fetched', this.messageService.messages);
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
+  deleteDuplicateMessage(partialMessage: string, arr: string[]) {
+    arr.forEach(element => {
+      if (element.includes(partialMessage)) {
+        console.log('deleting element');
+        const index: number = arr.indexOf[element];
+        arr.splice(index, 1);
+      }
+    });
+  }
   constructor(private messageService: MessageService) { }
 }
