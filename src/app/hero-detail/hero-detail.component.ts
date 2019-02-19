984b3f648984ec93918cad7d65a59da8a1db92b0
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-02-19 17:53:41 +0200

    Bla

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,7 +28,8 @@ export class HeroDetailComponent implements OnInit {
 //#region Methods
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
@@ -36,7 +37,8 @@ export class HeroDetailComponent implements OnInit {
   }
 
   save(): void {
-    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
   }
 
 
